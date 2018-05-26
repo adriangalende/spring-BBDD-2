ded78fc19ae4b98b91a2f4225846d613201a6955
Adrian Galende <adriangalende@gmail.com>
2018-05-26 13:58:11 +0200

    test_repositori_es_component

--- src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -2,11 +2,15 @@ package org.formacio.setmana2.repositori;
 
 import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+@Component
+@Transactional
 public class RepositoriEscola {
 
 	
