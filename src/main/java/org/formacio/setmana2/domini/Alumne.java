5572424284fd86594d8328a370499772baf79006
Adrian Galende <adriangalende@gmail.com>
2018-05-26 13:56:48 +0200

    test_equals_alumne y tes_equals_alumne_subtipus

--- src/main/java/org/formacio/setmana2/domini/Alumne.java
+++ src/main/java/org/formacio/setmana2/domini/Alumne.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name="t_alumnes")
@@ -26,5 +27,20 @@ public class Alumne {
 	public void setEdat(int edat) {
 		this.edat = edat;
 	}
-	
+
+	//Equals y gethash
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Alumne)) return false;
+		Alumne alumne = (Alumne) o;
+		return Objects.equals(getNom(), alumne.getNom());
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(getNom());
+	}
 }
